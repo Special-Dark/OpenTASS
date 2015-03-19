6f18e05ea49066fedca6320627a702b2c7d53739
Utopia <lanniaoershi@gmail.com>
2015-03-19 18:03:40 +0000

    Optimize view logic and improve performance

--- C/getSensorData.c
+++ C/getSensorData.c
@@ -9,16 +9,17 @@
 #include<stdio.h>
 #include<wiringSerial.h>
 #include<signal.h>
+#include<string.h>
 
 #define DEVICE "/dev/ttyAMA0"
 #define BAUD 9600
 #define true 1
 #define false 0
-#define DEBUG  0 // still have bugs when set to 1
-// #define G_scale 32768*16
-// #define W_scale 32768*2000
-#define G_scale 1
-#define W_scale 1
+#define DEBUG  "--debug"
+#define G_scale 32768*16
+#define W_scale 32768*2000
+//#define G_scale 1
+//#define W_scale 1
 
 static int dataFlow = 0;
 static short data8bit = 0;
@@ -26,28 +27,15 @@ static short dataBuffer[88];
 static short dataCount = 0;
 static short Gdata[3] = {0};
 static short Wdata[3] = {0};
+static float G[3] = {0};
+static float W[3] = {0};
 
 short readSensorData();
 int openSerialPort(char *device, int baud);
 void decodeDataTo16Bit(short data);
-void doNothing();
-
-void signalHandler(int sig) {
-	printf("\033[?25h");
-	printf("\nexit!\n");
-	exit(0);
-}
-int main() {
-
-	float G[3] = {0};
-	float W[3] = {0};
-
-	printf("\033[?25l");
-	signal(SIGINT,signalHandler);
-	dataFlow = openSerialPort(DEVICE, BAUD);
-
+void showFakeGUI();
+void mainProcess() {
 	while (true) {
-		//printf("data = %d\n", readSensorData());
 		decodeDataTo16Bit(readSensorData());
 		
 		G[0] = (float) Gdata[0] / G_scale;
@@ -57,17 +45,58 @@ int main() {
 		W[0] = (float) Wdata[0] / W_scale;
 		W[1] = (float) Wdata[1] / W_scale;
 		W[2] = (float) Wdata[2] / W_scale;
+	}
+}
 
-		printf("G = x=%.3f, y=%.3f, z=%.3f  ----  ", G[0],G[1],G[2]);
-		printf("W = x=%.3f, y=%.3f, z=%.3f\r", W[0],W[1],W[2]);
+void mainProcessWithDebug() {
+	while (true) {
+		decodeDataTo16Bit(readSensorData());
 		
-		if (DEBUG == true) {
-			doNothing();
-		}
+		G[0] = (float) Gdata[0] / G_scale;
+		G[1] = (float) Gdata[1] / G_scale;
+		G[2] = (float) Gdata[2] / G_scale;
+
+		W[0] = (float) Wdata[0] / W_scale;
+		W[1] = (float) Wdata[1] / W_scale;
+		W[2] = (float) Wdata[2] / W_scale;
+		showFakeGUI();
+	}
+}
+void signalHandler(int sig) {
+	printf("\033[?25h"); // set cursor show 
+	printf("\nexit!\n");
+	exit(0);
+}
+int main(int argc, char *argv[]) {
+	int mode = 0;
+
+	printf("Start runing...\n");
+	signal(SIGINT,signalHandler);
+	dataFlow = openSerialPort(DEVICE, BAUD);
+	if (argc == 2 && (strcmp(argv[1], DEBUG) == 0)) {
+		mode = 1;
+	} else {
+		mode = 0;
+	}
+
+	switch (mode) {
+		case 0 : 
+			  mainProcess();
+			  break;
+		case 1 :
+			  mainProcessWithDebug();
+			  break;
+		default : 
+			  return -1;
 	}
 	return 0;
 }
 
+void showFakeGUI() {
+	printf("G: x = %8.3f\n   y = %8.3f\n   z = %8.3f\n\nW: x = %8.3f\n   y = %8.3f\n   z = %8.3f\n", G[0],G[1],G[2],W[0],W[1],W[2]);
+	printf("\033c"); // set clear screen
+	printf("\033[?25l"); // set cursor hide
+}
 
 short readSensorData() {
 	data8bit = serialGetchar(dataFlow);
@@ -105,6 +134,3 @@ void decodeDataTo16Bit(short data) {
 
 }
 
-void doNothing() {
-	// do nothing
-}
